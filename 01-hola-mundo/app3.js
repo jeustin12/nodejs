be729b1adce47f66fbbae0d5d534ae388d616a99
justin <jeustinespinoza12@gmail.com>
2021-07-22 14:55:25 -0600

    hola mundo

---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,15 @@
+console.log('inicio');
+
+setTimeout(() => {
+    console.log('primer time');
+}, 3000);
+
+setTimeout(() => {
+    console.log('2 time');
+}, 0);
+
+setTimeout(() => {
+    console.log('3 time');
+}, 0);
+
+console.log('fin');
