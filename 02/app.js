1e18f15bd19a277513a42ab0f1c71cafeb0e3a8a
justin <jeustinespinoza12@gmail.com>
2021-07-23 12:18:29 -0600

    q

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,9 @@
+const { CrearTabla } = require('./utils/multiplicar');
 console.clear();
-const base = 8;
+
+const [, , arg3 = 'base=5'] = process.argv;
+const [, base = 5] = arg3.split('=');
+
+CrearTabla(base)
+    .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+    .catch((err) => console.log(err));
--- 02/utils/multiplicar.js
+++ 02/utils/multiplicar.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 
-const CrearTabla = (base = 5) => {
-    let salida = '';
+const CrearTabla = async (base = 5) => {
+    try {
+        let salida = '';
 
-    for (let i = 1; i <= 10; i++) {
-        salida += `${base} X ${i} es igual a ${base * i}\n`;
+        for (let i = 1; i <= 10; i++) {
+            salida += `${base} X ${i} es igual a ${base * i}\n`;
+        }
+
+        fs.writeFileSync(`tablas de multiplicar del ${base}.txt`, salida);
+
+        return `tabla del ${base}.txt creada`;
+    } catch (error) {
+        throw error;
     }
-    fs.writeFileSync(`tablas de multiplicar del ${base}.txt`, salida);
-
-    console.log(`tabla del ${base}.txt creada`);
 };
 
-module.exports = { CrearTabla };
+module.exports = {
+    CrearTabla,
+};
